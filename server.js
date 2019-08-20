6f34a906e5d5e210eed5de223279227a03434d6d
allisonbusse <allisonbusse@gmail.com>
2019-08-20 11:17:31 -0700

    got weather working

--- server.js
+++ server.js
@@ -53,18 +53,38 @@ const darkSky = require('./data/darksky.json');
 
 function getForcastTime(/*location*/) {
     //api call will go here
-    return toLocation(darkSky);
+    return toWeather(darkSky);
 }
 
-function toLocation(/*darkSky*/) {
+function toWeather(/*darkSky*/) {
     const firstResult = geoData.results[0];
-    const geometry = firstResult.geometry;
     
-    return {
+    return [{
         formatted_query: firstResult.formatted_address,
         forecast: darkSky.daily.data[0].summary,
         time: darkSky.daily.data[0].time
-    };
+    },
+    {
+        formatted_query: firstResult.formatted_address,
+        forecast: darkSky.daily.data[1].summary,
+        time: darkSky.daily.data[1].time
+    },
+    {
+        formatted_query: firstResult.formatted_address,
+        forecast: darkSky.daily.data[2].summary,
+        time: darkSky.daily.data[2].time
+    },
+    {
+        formatted_query: firstResult.formatted_address,
+        forecast: darkSky.daily.data[3].summary,
+        time: darkSky.daily.data[3].time
+    },
+    {
+        formatted_query: firstResult.formatted_address,
+        forecast: darkSky.daily.data[4].summary,
+        time: darkSky.daily.data[4].time
+    }
+    ];
 }
 
 app.listen(PORT, () => {
